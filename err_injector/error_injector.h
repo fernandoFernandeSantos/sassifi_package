b8484a1a0d9684d5a1c0aec92530af5002b4672b
Fernando <f_157@hotmail.com>
2017-05-31 17:08:29 -0300

    done

--- err_injector/error_injector.h
+++ err_injector/error_injector.h
@@ -1,29 +1,29 @@
 /* Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*  * Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution.
-*  * Neither the name of NVIDIA CORPORATION nor the names of its
-*    contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-* PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef __SASSIFI_STRUCTS___
 #define __SASSIFI_STRUCTS___
@@ -53,95 +53,101 @@
 
 // List of the Bit Flip Models (BFMs)
 enum BFM {
-	FLIP_SINGLE_BIT = 0, 
-	FLIP_TWO_BITS, // flip two adjacent bits
+	FLIP_SINGLE_BIT = 0, FLIP_TWO_BITS, // flip two adjacent bits
 	RANDOM_VALUE,  // no random value error for CC, PR
 	ZERO_VALUE,
 	// The following models inject the same pattern in destination registers in
 	// all the active threads in a warp 
-	WARP_FLIP_SINGLE_BIT, 
-	WARP_FLIP_TWO_BITS, 
-	WARP_RANDOM_VALUE, 
-	WARP_ZERO_VALUE, 
-	NUM_BFM_TYPES 
+	WARP_FLIP_SINGLE_BIT,
+	WARP_FLIP_TWO_BITS,
+	WARP_RANDOM_VALUE,
+	WARP_ZERO_VALUE,
+	NUM_BFM_TYPES
 };
 
 // Arch state error injection sites instruction group IDs
 // SASSIFI-user-guide summarizes these groups
 enum INST_TYPE {
-	GPR = 0, 
-	CC, 
-	PR, 
-	STORE_OP, 
-	IADD_IMUL_OP, 
-	FADD_FMUL_OP, 
-	DADD_DMUL_OP, 
-	MAD_OP, 
-	FFMA_OP, 
-	DFMA_OP, 
-	SETP_OP, 
-	LDS_OP, 
-	LD_OP, 
-	MISC_OP, 
+	GPR = 0,
+	CC,
+	PR,
+	STORE_OP,
+	IADD_IMUL_OP,
+	FADD_FMUL_OP,
+	DADD_DMUL_OP,
+	MAD_OP,
+	FFMA_OP,
+	DFMA_OP,
+	SETP_OP,
+	LDS_OP,
+	LD_OP,
+	MISC_OP,
 	NUM_INST_TYPES
 };
 
-const char *instCatName[NUM_INST_TYPES] = {
-	"GPR", "CC", "PR", "STORE_OP", 
-	"IADD_IMUL_OP", "FADD_FMUL_OP", "DADD_DMUL_OP", "MAD_OP", 
-	"FMA_OP", "DMA_OP", "SETP_OP", "LDS_OP", "LD_OP",
-	"MISC_OP"
-};
+const char *instCatName[NUM_INST_TYPES] = { "GPR", "CC", "PR", "STORE_OP",
+		"IADD_IMUL_OP", "FADD_FMUL_OP", "DADD_DMUL_OP", "MAD_OP", "FMA_OP",
+		"DMA_OP", "SETP_OP", "LDS_OP", "LD_OP", "MISC_OP" };
 
 // Instruction groupings
 // Input: opcode
 // Output: the instruction group the opcode belongs to 
-__device__ int get_op_category (int op) {
+__device__ int get_op_category(int op) {
 	switch (op) {
-		 case SASSI_OP_IMUL:
-		 case SASSI_OP_IMUL32I:
-		 case SASSI_OP_ISCADD:
-		 case SASSI_OP_ISCADD32I:
-		 case SASSI_OP_IADD:
-		 case SASSI_OP_IADD3:
-		 case SASSI_OP_IADD32I: return IADD_IMUL_OP;
+	case SASSI_OP_IMUL:
+	case SASSI_OP_IMUL32I:
+	case SASSI_OP_ISCADD:
+	case SASSI_OP_ISCADD32I:
+	case SASSI_OP_IADD:
+	case SASSI_OP_IADD3:
+	case SASSI_OP_IADD32I:
+		return IADD_IMUL_OP;
 
-		 case SASSI_OP_DADD:
-		 case SASSI_OP_DMUL: return DADD_DMUL_OP;
+	case SASSI_OP_DADD:
+	case SASSI_OP_DMUL:
+		return DADD_DMUL_OP;
 
-		 case SASSI_OP_FADD:
-		 case SASSI_OP_FMUL:
-		 case SASSI_OP_FMUL32I:
-		 case SASSI_OP_FADD32I: return FADD_FMUL_OP;
+	case SASSI_OP_FADD:
+	case SASSI_OP_FMUL:
+	case SASSI_OP_FMUL32I:
+	case SASSI_OP_FADD32I:
+		return FADD_FMUL_OP;
 
-		 case SASSI_OP_XMAD:
-		 case SASSI_OP_IMAD:
-		 case SASSI_OP_IMAD32I:
-		 case SASSI_OP_IMADSP: return MAD_OP;
+	case SASSI_OP_XMAD:
+	case SASSI_OP_IMAD:
+	case SASSI_OP_IMAD32I:
+	case SASSI_OP_IMADSP:
+		return MAD_OP;
 
-		 case SASSI_OP_DFMA: return DFMA_OP;
+	case SASSI_OP_DFMA:
+		return DFMA_OP;
 
-		 case SASSI_OP_FFMA:
-		 case SASSI_OP_FFMA32I: return FFMA_OP;
+	case SASSI_OP_FFMA:
+	case SASSI_OP_FFMA32I:
+		return FFMA_OP;
 
-		 case SASSI_OP_PSETP:
-		 case SASSI_OP_ISETP:
-		 case SASSI_OP_FSETP:
-		 case SASSI_OP_DSETP:
-		 case SASSI_OP_CSETP: return SETP_OP;
+	case SASSI_OP_PSETP:
+	case SASSI_OP_ISETP:
+	case SASSI_OP_FSETP:
+	case SASSI_OP_DSETP:
+	case SASSI_OP_CSETP:
+		return SETP_OP;
 
-		 case SASSI_OP_LDSLK:
-		 case SASSI_OP_LDS_LDU:
-		 case SASSI_OP_LDS: return LDS_OP;
+	case SASSI_OP_LDSLK:
+	case SASSI_OP_LDS_LDU:
+	case SASSI_OP_LDS:
+		return LDS_OP;
 
-		 case SASSI_OP_LDG:
-		 case SASSI_OP_LDL:
-		 case SASSI_OP_LDLK:
-		 case SASSI_OP_LDU:
-		 case SASSI_OP_LD_LDU:
-		 case SASSI_OP_LD: return LD_OP;
+	case SASSI_OP_LDG:
+	case SASSI_OP_LDL:
+	case SASSI_OP_LDLK:
+	case SASSI_OP_LDU:
+	case SASSI_OP_LD_LDU:
+	case SASSI_OP_LD:
+		return LD_OP;
 
-		 default:  return MISC_OP; 
+	default:
+		return MISC_OP;
 	}
 }
 
@@ -186,58 +192,60 @@ __device__ void clear_mem_info() {
 
 // read value from memory based on the bidwidth (in bits here)
 __device__ uint128_t read_memory_value(int64_t address, int32_t bitwidth) {
-	uint128_t val; 
+	uint128_t val;
 	if (bitwidth == 32) { // most common case
-		val.values[0] = (uint64_t) *((uint32_t*)address);
-	} else if (bitwidth == 8) { 
-		val.values[0] = (uint64_t) *((uint8_t*)address);
-	} else if (bitwidth == 16) { 
-		val.values[0] = (uint64_t) *((uint16_t*)address);
+		val.values[0] = (uint64_t) * ((uint32_t*) address);
+	} else if (bitwidth == 8) {
+		val.values[0] = (uint64_t) * ((uint8_t*) address);
+	} else if (bitwidth == 16) {
+		val.values[0] = (uint64_t) * ((uint16_t*) address);
 	} else if (bitwidth == 64) {
-		val.values[0] = *((uint64_t*)address);
+		val.values[0] = *((uint64_t*) address);
 	} else if (bitwidth >= 128) {
-		val.values[0] =	(*((uint128_t*)address)).values[0];
-		val.values[1] =	(*((uint128_t*)address)).values[1];
+		val.values[0] = (*((uint128_t*) address)).values[0];
+		val.values[1] = (*((uint128_t*) address)).values[1];
 	}
 	return val;
 }
 
 // write value to memory based on the bidwidth (in bits here)
-__device__ void write_memory_value(int64_t address, uint128_t val, int32_t bitwidth) {
+__device__ void write_memory_value(int64_t address, uint128_t val,
+		int32_t bitwidth) {
 	if (bitwidth == 32) { // most common case
-		*((uint32_t*)address) = (uint32_t)val.values[0];
-	} else if (bitwidth == 8) { 
-		*((uint8_t*)address) = (uint8_t)val.values[0];
-	} else if (bitwidth == 16) { 
-		*((uint16_t*)address) = (uint16_t)val.values[0];
+		*((uint32_t*) address) = (uint32_t) val.values[0];
+	} else if (bitwidth == 8) {
+		*((uint8_t*) address) = (uint8_t) val.values[0];
+	} else if (bitwidth == 16) {
+		*((uint16_t*) address) = (uint16_t) val.values[0];
 	} else if (bitwidth == 64) {
-		*((uint64_t*)address) = (uint64_t)val.values[0];
+		*((uint64_t*) address) = (uint64_t) val.values[0];
 	} else if (bitwidth >= 128) {
-		((uint128_t*)address)->values[0] = val.values[0];
-		((uint128_t*)address)->values[1] = val.values[1];
+		((uint128_t*) address)->values[0] = val.values[0];
+		((uint128_t*) address)->values[1] = val.values[1];
 	}
 }
 
 // record memory value based on the bidwidth (in bits here)
 __device__ void record_memory_info(int64_t address, int32_t bitwidth) {
-	mem_info_d.bitwidth = bitwidth; 
-	mem_info_d.address = address; 
+	mem_info_d.bitwidth = bitwidth;
+	mem_info_d.address = address;
 
 	mem_info_d.value.values[0] = 0;
 	mem_info_d.value.values[1] = 0;
 
 	if (bitwidth == 32) { // most common case
-		mem_info_d.value.values[0] = (uint64_t) *((uint32_t*)address);
-		DEBUG_PRINT(INJ_DEBUG_HEAVY, "memAddr=%llx: value before=%llx\n", address, mem_info_d.value.values[0]);
-	} else if (bitwidth == 8) { 
-		mem_info_d.value.values[0] = (uint64_t) *((uint8_t*)address);
-	} else if (bitwidth == 16) { 
-		mem_info_d.value.values[0] = (uint64_t) *((uint16_t*)address);
+		mem_info_d.value.values[0] = (uint64_t) * ((uint32_t*) address);
+		DEBUG_PRINT(INJ_DEBUG_HEAVY, "memAddr=%llx: value before=%llx\n",
+				address, mem_info_d.value.values[0]);
+	} else if (bitwidth == 8) {
+		mem_info_d.value.values[0] = (uint64_t) * ((uint8_t*) address);
+	} else if (bitwidth == 16) {
+		mem_info_d.value.values[0] = (uint64_t) * ((uint16_t*) address);
 	} else if (bitwidth == 64) {
-		mem_info_d.value.values[0] = *((uint64_t*)address);
+		mem_info_d.value.values[0] = *((uint64_t*) address);
 	} else if (bitwidth >= 128) {
-		mem_info_d.value.values[0] =	(*((uint128_t*)address)).values[0];
-		mem_info_d.value.values[1] =	(*((uint128_t*)address)).values[1];
+		mem_info_d.value.values[0] = (*((uint128_t*) address)).values[0];
+		mem_info_d.value.values[1] = (*((uint128_t*) address)).values[1];
 	}
 }
 
@@ -252,7 +260,7 @@ __managed__ reg_info_t tmp_store_reg_info_d;
 __managed__ reg_info_t reg_info_d[MAX_REGS_PERINST];
 
 __device__ void clear_register_info() {
-	for (int i=0; i<MAX_REGS_PERINST; i++) {
+	for (int i = 0; i < MAX_REGS_PERINST; i++) {
 		reg_info_d[i].address = -1;
 		reg_info_d[i].value.asInt = -1;
 	}
@@ -265,18 +273,20 @@ __device__ void clear_register_info() {
 // returns an integer idex of the destination using 
 // inputs, injection seed (opID) and number of operands (numOps)
 __device__ int32_t get_int_inj_id(int32_t numOps, float opID) {
-	return (int32_t)(numOps*opID);
+	return (int32_t)(numOps * opID);
 }
 
-__device__ inline  bool is_store_inst(SASSICoreParams* ap, SASSIMemoryParams *mp) {
-	if (ap->IsMem()) {// is memory instruction?
+__device__ inline bool is_store_inst(SASSICoreParams* ap,
+		SASSIMemoryParams *mp) {
+	if (ap->IsMem()) { // is memory instruction?
 		if (mp->IsStore()) {
 			return true;
-		} 
+		}
 	}
 	return false;
 }
-__device__ inline bool is_load_inst(SASSICoreParams* ap, SASSIMemoryParams *mp) {
+__device__ inline bool is_load_inst(SASSICoreParams* ap,
+		SASSIMemoryParams *mp) {
 	if (ap->IsMem()) { // is memory instruction?
 		if (mp->IsLoad()) {
 			return true;
@@ -291,36 +301,38 @@ __device__ inline bool has_dest_GPR(SASSIRegisterParams *rp) {
 }
 
 // returns true if the instruction writes to Condition Code (CC) register
-__device__ inline bool has_dest_CC(SASSIRegisterParams *rp)  {
-	return rp->IsCCDefined(); 
+__device__ inline bool has_dest_CC(SASSIRegisterParams *rp) {
+	return rp->IsCCDefined();
 }
 
 // returns true if the instruction writes to a Predicate register (PR)
-__device__ inline bool has_dest_PR(SASSIRegisterParams *rp)  {
-	return (rp->GetPredicateDstMask() != 0); 
+__device__ inline bool has_dest_PR(SASSIRegisterParams *rp) {
+	return (rp->GetPredicateDstMask() != 0);
 }
 
 // returns true if the instruction has either GPR, CC, or PR as a destination register
 __device__ bool has_dest_reg(SASSIRegisterParams *rp) {
-	return (has_dest_GPR(rp) | has_dest_PR(rp) | has_dest_CC(rp)); 
+	return (has_dest_GPR(rp) | has_dest_PR(rp) | has_dest_CC(rp));
 }
 
 // flatten thread id
 __device__ int get_flat_tid() {
-  int tid_b = threadIdx.x + (blockDim.x * (threadIdx.y + (threadIdx.z * blockDim.y))); // thread id within a block
-  int bid = blockIdx.x + (gridDim.x * (blockIdx.y + (blockIdx.z * gridDim.y))); // block id 
+	int tid_b = threadIdx.x
+			+ (blockDim.x * (threadIdx.y + (threadIdx.z * blockDim.y))); // thread id within a block
+	int bid = blockIdx.x
+			+ (gridDim.x * (blockIdx.y + (blockIdx.z * gridDim.y))); // block id
 	int tid = tid_b + (bid * blockDim.x * blockDim.y * blockDim.z);
 	return tid;
 }
 
 // return the format in which the instruction profile is printed in the sassifi-inst-counts.txt file 
-std::string  get_profile_format() {
+std::string get_profile_format() {
 	std::string first_line = "kName:kernelCount";
-	for (int i=0; i<NUM_INST_TYPES; i++) 
-		first_line += ":" + (std::string)instCatName[i] ;
+	for (int i = 0; i < NUM_INST_TYPES; i++)
+		first_line += ":" + (std::string) instCatName[i];
 	first_line += ":opWillNotExecuteCount";
-	for (int i=0; i<SASSI_NUM_OPCODES; i++)
-		first_line += ":" + (std::string)SASSIInstrOpcodeStrings[i] ;
+	for (int i = 0; i < SASSI_NUM_OPCODES; i++)
+		first_line += ":" + (std::string) SASSIInstrOpcodeStrings[i];
 	return first_line + "\n";
 }
 
@@ -334,27 +346,28 @@ __managed__ unsigned long long injCountersInstType[NUM_INST_TYPES]; // count ins
 __managed__ unsigned long long opCounters[SASSI_NUM_OPCODES]; // count instruction based on opcode
 __managed__ unsigned long long opWillNotExecuteCount; // counter for instructions that will not execute 
 
-__device__ inline void profile_instructions(SASSICoreParams* cp, SASSIMemoryParams* mp, SASSIRegisterParams* rp) {
+__device__ inline void profile_instructions(SASSICoreParams* cp,
+		SASSIMemoryParams* mp, SASSIRegisterParams* rp) {
 
-	atomicAdd(&injCountersInstType[GPR], (unsigned long long)has_dest_GPR(rp)); 
+	atomicAdd(&injCountersInstType[GPR], (unsigned long long) has_dest_GPR(rp));
 
 	if (has_dest_CC(rp)) {
 		//atomicAdd(&injCountersInstType[CC], (unsigned long long)has_dest_CC(rp)); 
-		atomicAdd(&injCountersInstType[CC], 1LL); 
+		atomicAdd(&injCountersInstType[CC], 1LL);
 	}
-	if (has_dest_PR(rp)) { 
+	if (has_dest_PR(rp)) {
 		//atomicAdd(&injCountersInstType[PR], (unsigned long long)has_dest_PR(rp)); 
-		atomicAdd(&injCountersInstType[PR], 1LL); 
+		atomicAdd(&injCountersInstType[PR], 1LL);
 	}
 	if (is_store_inst(cp, mp)) {
 		// atomicAdd(&injCountersInstType[STORE_OP], (unsigned long long)is_store_inst(cp, mp)); 
-		atomicAdd(&injCountersInstType[STORE_OP], 1LL); 
+		atomicAdd(&injCountersInstType[STORE_OP], 1LL);
 	}
 
 	int op = cp->GetOpcode();
 	atomicAdd(&opCounters[op], 1LL);
 
-	atomicAdd(&injCountersInstType[get_op_category(op)], 1LL); 
+	atomicAdd(&injCountersInstType[get_op_category(op)], 1LL);
 }
 
 __device__ inline void profile_will_not_execute_instructions() {
@@ -362,7 +375,7 @@ __device__ inline void profile_will_not_execute_instructions() {
 }
 
 void reset_profiling_counters() {
-	injCounterAllInsts = 0; 
+	injCounterAllInsts = 0;
 	opWillNotExecuteCount = 0;
 	bzero(injCountersInstType, sizeof(injCountersInstType));
 	bzero(opCounters, sizeof(opCounters));
--- scripts/common_functions.py
+++ scripts/common_functions.py
@@ -29,70 +29,76 @@
 import sys, re, string, os, operator, math, datetime, random
 import common_params as cp
 
+
 # parse the file with inst count info per thread and create a tid->inst_count map
 def read_inst_counts(app_dir, app):
-	countList = []
-	fName = app_dir + "/sassifi-inst-counts.txt"
-	if not os.path.exists(fName):
-		print "%s file not found!" %fName 
-		return countList
+    countList = []
+    fName = app_dir + "/sassifi-inst-counts.txt"
+    if not os.path.exists(fName):
+        print "%s file not found!" % fName
+        return countList
 
-	f = open(fName, "r")
-	next(f) # Skip first line. This line has just the format.
-	for line in f:
-		line = line.rstrip()
-		countList.append(line.split(":"))
-		#countList.append([kName, int(kCount), int(storeCount), int(loadCount), int(gpCount), int(ccCount), int(prCount)])
-	f.close()
+    f = open(fName, "r")
+    next(f)  # Skip first line. This line has just the format.
+    for line in f:
+        line = line.rstrip()
+        countList.append(line.split(":"))
+    # countList.append([kName, int(kCount), int(storeCount), int(loadCount), int(gpCount), int(ccCount), int(prCount)])
+    f.close()
 
-	return countList 
+    return countList
+
 
 # Sample format from sassifi-inst-counts.txt - kName:kernelCount:GPR:CC:PR:STORE_OP:IADD_IMUL_OP:FADD_FMUL_OP:DADD_DMUL_OP:MAD_OP:FFMA_OP:DFMA_OP:SETP_OP:LDS_OP:LD_OP:MISC_OP:opWillNotExecuteCount::ATOMS:B2R:BAR:BFE:BFI:BPT:BRA:BRK:BRX:CAL:CAS:CCTL:CCTLL:CCTLT:CONT:CS2R:CSET:CSETP:DADD:DEPBAR:DFMA:DMNMX:DMUL:DSET:DSETP:EXIT:F2F:F2I:FADD:FADD32I:FCHK:FCMP:FFMA:FFMA32I:FLO:FMNMX:FMUL:FMUL32I:FSET:FSETP:FSWZ:FSWZADD:I2F:I2I:IADD:IADD3:IADD32I:ICMP:IMAD:IMAD32I:IMADSP:IMNMX:IMUL:IMUL32I:ISAD:ISCADD:ISCADD32I:ISET:ISETP:JCAL:JMX:LD:LDC:LDG:LDL:LDLK:LDS:LDSLK:LDS_LDU:LDU:LD_LDU:LEA:LEPC:LONGJMP:LOP:LOP3:LOP32I:MEMBAR:MOV:MUFU:NOP:P2R:PBK:PCNT:PEXIT:PLONGJMP:POPC:PRET:PRMT:PSET:PSETP:R2B:R2P:RED:RET:RRO:S2R:SEL:SHF:SHFL:SHL:SHR:SSY:ST:STG:STL:STS:STSCUL:STSUL:STUL:SUATOM:SUBFM:SUCLAMP:SUEAU:SULD:SULDGA:SULEA:SUQ:SURED:SUST:SUSTGA:SYNC:TEX:TEXDEPBAR:TEXS:TLD:TLD4:TLD4S:TLDS:TXQ:UNMAPPED:USER_DEFINED:VMNMX:VOTE:XMAD
 def get_inst_count_format():
-	ret_str = "kName:kernelCount"
-	for s in cp.IGID_STR:
-		ret_str += ":" + s
-	ret_str += ":WILL_NOT_EXECUTE"
+    ret_str = "kName:kernelCount"
+    for s in cp.IGID_STR:
+        ret_str += ":" + s
+    ret_str += ":WILL_NOT_EXECUTE"
 
-	ret_str += ":ATOM:ATOMS:B2R:BAR:BFE:BFI:BPT:BRA:BRK:BRX:CAL:CAS:CCTL:CCTLL:CCTLT:CONT:CS2R:CSET:CSETP:DADD:DEPBAR:DFMA:DMNMX:DMUL:DSET:DSETP:EXIT:F2F:F2I:FADD:FADD32I:FCHK:FCMP:FFMA:FFMA32I:FLO:FMNMX:FMUL:FMUL32I:FSET:FSETP:FSWZ:FSWZADD:I2F:I2I:IADD:IADD3:IADD32I:ICMP:IMAD:IMAD32I:IMADSP:IMNMX:IMUL:IMUL32I:ISAD:ISCADD:ISCADD32I:ISET:ISETP:JCAL:JMX:LD:LDC:LDG:LDL:LDLK:LDS:LDSLK:LDS_LDU:LDU:LD_LDU:LEA:LEPC:LONGJMP:LOP:LOP3:LOP32I:MEMBAR:MOV:MUFU:NOP:P2R:PBK:PCNT:PEXIT:PLONGJMP:POPC:PRET:PRMT:PSET:PSETP:R2B:R2P:RED:RET:RRO:S2R:SEL:SHF:SHFL:SHL:SHR:SSY:ST:STG:STL:STS:STSCUL:STSUL:STUL:SUATOM:SUBFM:SUCLAMP:SUEAU:SULD:SULDGA:SULEA:SUQ:SURED:SUST:SUSTGA:SYNC:TEX:TEXDEPBAR:TEXS:TLD:TLD4:TLD4S:TLDS:TXQ:UNMAPPED:USER_DEFINED:VMNMX:VOTE:XMAD"
+    ret_str += ":ATOM:ATOMS:B2R:BAR:BFE:BFI:BPT:BRA:BRK:BRX:CAL:CAS:CCTL:CCTLL:CCTLT:CONT:CS2R:CSET:CSETP:DADD:DEPBAR:DFMA:DMNMX:DMUL:DSET:DSETP:EXIT:F2F:F2I:FADD:FADD32I:FCHK:FCMP:FFMA:FFMA32I:FLO:FMNMX:FMUL:FMUL32I:FSET:FSETP:FSWZ:FSWZADD:I2F:I2I:IADD:IADD3:IADD32I:ICMP:IMAD:IMAD32I:IMADSP:IMNMX:IMUL:IMUL32I:ISAD:ISCADD:ISCADD32I:ISET:ISETP:JCAL:JMX:LD:LDC:LDG:LDL:LDLK:LDS:LDSLK:LDS_LDU:LDU:LD_LDU:LEA:LEPC:LONGJMP:LOP:LOP3:LOP32I:MEMBAR:MOV:MUFU:NOP:P2R:PBK:PCNT:PEXIT:PLONGJMP:POPC:PRET:PRMT:PSET:PSETP:R2B:R2P:RED:RET:RRO:S2R:SEL:SHF:SHFL:SHL:SHR:SSY:ST:STG:STL:STS:STSCUL:STSUL:STUL:SUATOM:SUBFM:SUCLAMP:SUEAU:SULD:SULDGA:SULEA:SUQ:SURED:SUST:SUSTGA:SYNC:TEX:TEXDEPBAR:TEXS:TLD:TLD4:TLD4S:TLDS:TXQ:UNMAPPED:USER_DEFINED:VMNMX:VOTE:XMAD"
 
-	return ret_str
+    return ret_str
 
-#return total number of instructions of each type or opcode
+
+# return total number of instructions of each type or opcode
 def get_total_counts(countList):
-	length = get_inst_count_format().count(':')-1
-	total_icounts = [0] * length
-	for l in countList:
-		for i in range(length):
-			total_icounts[i] += int(l[2+i])
-	return total_icounts
+    length = get_inst_count_format().count(':') - 1
+    total_icounts = [0] * length
+    for l in countList:
+        for i in range(length):
+            total_icounts[i] += int(l[2 + i])
+    return total_icounts
 
-# return total number of instructions in the countList 
+
+# return total number of instructions in the countList
 def get_total_insts(countList, with_will_not_execute):
-	total = 0
-	for l in countList:
-		# 3: 1 for kname, 1 for kcount and 1 for WILL NOT EXECUTE instruction count
-		# 2: 1 for kname, 1 for kcount 
-		start = cp.NUM_INST_TYPES+2 if with_will_not_execute else cp.NUM_INST_TYPES+3
-		for i in range(start, len(countList[0])): 
-			total += int(l[i])
-	return total
+    total = 0
+    for l in countList:
+        # 3: 1 for kname, 1 for kcount and 1 for WILL NOT EXECUTE instruction count
+        # 2: 1 for kname, 1 for kcount
+        start = cp.NUM_INST_TYPES + 2 if with_will_not_execute else cp.NUM_INST_TYPES + 3
+        for i in range(start, len(countList[0])):
+            total += int(l[i])
+    return total
+
 
 def get_rf_injection_site_info(countList, inj_num, with_will_not_execute):
-	start = 0
-	for item in countList:
-		st = cp.NUM_INST_TYPES+2 if with_will_not_execute else cp.NUM_INST_TYPES+3
-		total = sum(int(item[i]) for i in range(st, len(countList[0]))) # total number of instructions in this kernel
-		if start <= inj_num < start + total:
-			return [item[0], item[1], inj_num-start] # return [kname, kcount, inj_num in this kernel]
-		start += total
-	return ["", -1, -1]
+    start = 0
+    for item in countList:
+        st = cp.NUM_INST_TYPES + 2 if with_will_not_execute else cp.NUM_INST_TYPES + 3
+        total = sum(int(item[i]) for i in range(st, len(countList[0])))  # total number of instructions in this kernel
+        if start <= inj_num < start + total:
+            return [item[0], item[1], inj_num - start]  # return [kname, kcount, inj_num in this kernel]
+        start += total
+    return ["", -1, -1]
+
 
 def get_injection_site_info(countList, inj_num, igid):
-	start = 0
-	idx = igid + 2
-	for item in countList:
-		if start <= inj_num < start + int(item[idx]):
-			return [item[0], item[1], inj_num-start] # return [kname, kcount, inj_num in this kernel]
-		start += int(item[idx])
-	return ["", -1, -1]
+    start = 0
+    idx = igid + 2
+    for item in countList:
+        if start <= inj_num < start + int(item[idx]):
+            return [item[0], item[1], inj_num - start]  # return [kname, kcount, inj_num in this kernel]
+        start += int(item[idx])
+    return ["", -1, -1]
