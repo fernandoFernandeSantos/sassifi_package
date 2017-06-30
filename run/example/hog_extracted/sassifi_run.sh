6ac32a0f0ec3f8e5a053cd853ccb2e0da8471013
reliabilitylogdata <reliabilitylogdata@github.com>
2017-06-30 12:02:17 -0300

    hog

--- run/example/hog_extracted/sassifi_run.sh
+++ run/example/hog_extracted/sassifi_run.sh
@@ -1,3 +1,4 @@
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-${BIN_DIR}/hog ${APP_DIR}/1x_pedestrians.jpg --dst_data ${APP_DIR}/GOLD_1x.data --iterations 1 > stdout.txt 2> stderr.txt 
+DATASET =/home/carol/radiation-benchmarks/data/networks_img_list/caltech.pedestrians.critical.1K.txt
+${BIN_DIR}/hog ${DATASET} --iterations 1 > stdout.txt 2> stderr.txt
