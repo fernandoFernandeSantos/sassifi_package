71abc30b0bd1f4657832ea0aed2e7bc432b1c856
reliabilitylogdata <reliabilitylogdata@github.com>
2017-06-30 13:56:50 -0300

    lava run file

--- run/example/lava/sassifi_run.sh
+++ run/example/lava/sassifi_run.sh
@@ -1,10 +1,4 @@
-# SASSI/CUDA
-CUDA_BASE_DIR=/usr/local/sassi7
-CUDA_LIB_DIR=${CUDA_BASE_DIR}/lib64
-CUDA_BIN_DIR=${CUDA_BASE_DIR}/bin
-CUPTI_LIB_DIR=${CUDA_BASE_DIR}/extras/CUPTI/lib64
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CUDA_LIB_DIR}:${CUPTI_LIB_DIR}
-
-
-${BIN_DIR}/lava -boxes=10 -input_distances=${APP_DIR}/input_distances_10 -input_charges=${APP_DIR}/input_charges_10 -output_gold=${APP_DIR}/GOLD -iterations=1 -streams=1 > stdout.txt 2> stderr.txt 
+${BIN_DIR}/lava -boxes=10 -input_distances=${APP_DIR}/input_distances_10 -input_charges=${APP_DIR}/input_charges_10 -output_gold=${APP_DIR}/GOLD -iterations=1 -streams=1 > stdout.txt 2> stderr.txt
