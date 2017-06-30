68e516524a9dd20d04550f0e27f76110a054aafb
reliabilitylogdata <reliabilitylogdata@github.com>
2017-06-30 13:46:48 -0300

    hotspot run

--- run/example/hotspot/sassifi_run.sh
+++ run/example/hotspot/sassifi_run.sh
@@ -1,3 +1,3 @@
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-${BIN_DIR}/hotspot -size=1024 -sim_time=1 -streams=1 -temp_file=${APP_DIR}/temp_1024 -power_file=${APP_DIR}/power_1024 -gold_file=${APP_DIR}/gold_1024_1 -iterations=1 -verbose > stdout.txt 2> stderr.txt 
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
+${BIN_DIR}/hotspot -size=1024 -sim_time=1 -streams=1 -temp_file=${APP_DIR}/temp_1024 -power_file=${APP_DIR}/power_1024 -gold_file=${APP_DIR}/gold_1024_1 -iterations=1 > stdout.txt 2> stderr.txt
