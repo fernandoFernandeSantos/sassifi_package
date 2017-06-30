a365e1ea3ef5fdb57987e31fe1ed5593409ad3a9
Fernando <fernandofernandesantos@gmail.com>
2017-06-30 13:35:03 -0300

    run files

--- run/example/accl/sassifi_run.sh
+++ run/example/accl/sassifi_run.sh
@@ -1,10 +1,11 @@
 # SASSI/CUDA
-CUDA_BASE_DIR=/usr/local/sassi7
-CUDA_LIB_DIR=${CUDA_BASE_DIR}/lib64
-CUDA_BIN_DIR=${CUDA_BASE_DIR}/bin
-CUPTI_LIB_DIR=${CUDA_BASE_DIR}/extras/CUPTI/lib64
+#CUDA_BASE_DIR=/usr/local/sassi7
+#CUDA_LIB_DIR=${CUDA_BASE_DIR}/lib64
+#CUDA_BIN_DIR=${CUDA_BASE_DIR}/bin
+#CUPTI_LIB_DIR=${CUDA_BASE_DIR}/extras/CUPTI/lib64
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CUDA_LIB_DIR}:${CUPTI_LIB_DIR}
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CUDA_LIB_DIR}:${CUPTI_LIB_DIR}
-
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
 
 ${BIN_DIR}/accl 2 1  ${APP_DIR}/2Frames.pgm ${APP_DIR}/GOLD_2Frames 1 -verbose > stdout.txt 2> stderr.txt 
--- run/example/hog_extracted/sassifi_run.sh
+++ run/example/hog_extracted/sassifi_run.sh
@@ -1,4 +1,6 @@
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-DATASET =/home/carol/radiation-benchmarks/data/networks_img_list/caltech.pedestrians.critical.1K.txt
-${BIN_DIR}/hog ${DATASET} --iterations 1 > stdout.txt 2> stderr.txt
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
+
+DATASET=/home/carol/radiation-benchmarks/data/networks_img_list/caltech.pedestrians.critical.1K.txt
+
+${BIN_DIR}/hog_extracted ${DATASET} --iterations 1 > stdout.txt 2> stderr.txt
