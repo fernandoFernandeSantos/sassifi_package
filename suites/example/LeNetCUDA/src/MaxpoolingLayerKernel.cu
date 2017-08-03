3deb5929d91257e77e4351484b6979bb48cbcda2
Fernando <f_157@hotmail.com>
2017-08-03 16:43:27 -0300

    fixing maxpool

--- suites/example/LeNetCUDA/src/MaxpoolingLayerKernel.cu
+++ suites/example/LeNetCUDA/src/MaxpoolingLayerKernel.cu
@@ -24,24 +24,24 @@ __device__ inline Pair get_max_loc_pair(size_t first, size_t second) {
 	return ret;
 }
 
-__device__ inline float max_in_(float_t *input_, Pair *max_loc,
+__device__ inline float max_in_cuda(float_t *input_, Pair *max_loc,
 		size_t in_width_, size_t in_height_, size_t in_index, size_t h_,
 		size_t w_, size_t out_index) {
 	float_t max_pixel = 0;
 	size_t tmp;
 
-#pragma unroll
-	for (size_t x = 0; x < MAXPOOL_SIZE; x++) {
-#pragma unroll
-		for (size_t y = 0; y < MAXPOOL_SIZE; y++) {
-			tmp = (in_index * in_width_ * in_height_) + ((h_ + y) * in_width_)
-					+ (w_ + x);
-			if (max_pixel < input_[tmp]) {
-				max_pixel = input_[tmp];
-				max_loc[out_index] = get_max_loc_pair(out_index, tmp);
-			}
-		}
-	}
+//#pragma unroll
+//	for (size_t x = 0; x < MAXPOOL_SIZE; x++) {
+//#pragma unroll
+//		for (size_t y = 0; y < MAXPOOL_SIZE; y++) {
+//			tmp = (in_index * in_width_ * in_height_) + ((h_ + y) * in_width_)
+//					+ (w_ + x);
+//			if (max_pixel < input_[tmp]) {
+//				max_pixel = input_[tmp];
+//				max_loc[out_index] = get_max_loc_pair(out_index, tmp);
+//			}
+//		}
+//	}
 	return max_pixel;
 }
 
@@ -72,8 +72,8 @@ __global__ void forward_maxpool_layer_kernel(float_t *input_, Pair *max_loc,
 	if ((out < out_depth_) && (h_ < in_height) && (w_ < in_width) && !(h_ % 2)
 			&& !(w_ % 2)) {
 		size_t index = get_out_index(out_width, out_height, out, h_, w_);
-//		output_[index] = max_in_(input_, max_loc, in_width, in_height, out, h_,
-//				w_, index);
+		output_[index] = max_in_cuda(input_, max_loc, in_width, in_height, out, h_,
+				w_, index);
 
 	}
 }
