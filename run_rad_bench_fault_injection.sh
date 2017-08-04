9e9122800d0320878ee1b21e68483330c60abf29
carol <fernandofernandesantos@gmail.com>
2017-08-04 21:32:45 +0000

    make cuda

--- run_rad_bench_fault_injection.sh
+++ run_rad_bench_fault_injection.sh
@@ -6,9 +6,9 @@ set -e
 BASE_DIR=/home/carol/SASSIFI/sassifi_package/suites/example
 
 #first build include dir loghelper
-make -C ${BASE_DIR}/include/ cuda
+make -C ${BASE_DIR}/include/
 
-for i in accl hog_extracted hotspot lava lud lulesh mergesort nw quicksort;
+for i in LeNetCUDA; #accl hotspot lava lud lulesh mergesort nw quicksort;
 do
     echo "###############################################################"
     echo "                     DOING FOR $i"
