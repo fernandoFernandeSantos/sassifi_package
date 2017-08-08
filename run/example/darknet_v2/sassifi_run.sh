e0ae7dffe56c4b282908c96aaf53bf69f2759837
reliabilitylogdata <reliabilitylogdata@github.com>
2017-08-07 22:19:04 -0300

    darknet fi

---
+++ run/example/darknet_v2/sassifi_run.sh
@@ -0,0 +1,6 @@
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:/usr/local/sassi7/extras/CUPTI/lib64/:/usr/local/sassi7/lib64/:$LD_LIBRARY_PATH
+
+RAD_DIR=/home/carol/radiation-benchmarks
+
+${BIN_DIR}/darknet test_radiation -d $RAD_DIR/data/darknet/fault_injection.csv -n 1 -s 1 -a 0 > stdout.txt 2> stderr.txt
