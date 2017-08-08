39807582798492ad34784fabad3cec241e9507a2
carol <fernandofernandesantos@gmail.com>
2017-08-08 01:52:17 +0000

    rum darknet

--- run_rad_bench_fault_injection.sh
+++ run_rad_bench_fault_injection.sh
@@ -8,7 +8,7 @@ BASE_DIR=/home/carol/SASSIFI/sassifi_package/suites/example
 #first build include dir loghelper
 make -C ${BASE_DIR}/include/
 
-for i in LeNetCUDA; #accl hotspot lava lud lulesh mergesort nw quicksort;
+for i in darknet_v2; #LeNetCUDA accl hotspot lava lud lulesh mergesort nw quicksort;
 do
     echo "###############################################################"
     echo "                     DOING FOR $i"
