c49bcb9e93704017f58e329b650dbfa27af0ed17
Your Name <you@example.com>
2019-08-22 12:58:27 -0300

    sassifi run

--- run/example/batched_mxm/sassifi_run.sh
+++ run/example/batched_mxm/sassifi_run.sh
@@ -1,4 +1,6 @@
 export PATH=/usr/local/cuda-7.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
-
-${BIN_DIR}/cuda_batched_mxm -size=128 -batch=32 -kernel_type=0 -input_a=${APP_DIR}/mxm_A_128_32.matrix -input_b=${APP_DIR}/mxm_B_128_32.matrix -gold=${APP_DIR}/mxm_GOLD_128_32.matrix -iterations=1  > stdout.txt 2> stderr.txt
+SIZE=112
+BATCH=8192
+KTYPE=0
+${BIN_DIR}/cuda_batched_mxm -size=${SIZE} -batch=${BATCH} -kernel_type=${KTYPE} -input_a=${APP_DIR}/mxm_A_${SIZE}_${BATCH}.matrix -input_b=${APP_DIR}/mxm_B_${SIZE}_${BATCH}.matrix -gold=${APP_DIR}/mxm_GOLD_${SIZE}_${BATCH}.matrix -iterations=1  > stdout.txt 2> stderr.txt
